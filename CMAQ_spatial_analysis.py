0f6183879b2884aeff99218d80a616b39556d5d3
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-05-22 10:38:56 -0700

    added basemap class instance

--- CMAQ_spatial_analysis.py
+++ CMAQ_spatial_analysis.py
@@ -10,35 +10,66 @@ purpose: spatial plot for CMAQ
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 import numpy as np
-from mpl_toolkits.basemap import Basemap as bm
+from mpl_toolkits.basemap import Basemap , cm
 
 # import input files
 #cmaq_file =
 mcip_file = '/Users/ehsan/Documents/Python_projects/USFS_fire/inputs/cmaq_inputs/GRIDDOT2D_161001'
 
 mcip_input = Dataset( mcip_file )
-#cmaq_input = Dataset( cmaq_input )
+#cmaq_input = data_meshset( cmaq_input )
 
 # some info
 print('-> MCIP file dimensions: %s' %str( mcip_input['LATD'].dimensions ) )
 print('-> shape of each dimension: %s' %( str(mcip_input['LATD'].shape ) ))
+print('-> plotting the data...')
 
 # extract lat and lon parameteres
-lat_list = np.array( mcip_input['LATD'][0][0][:][:] ) # select only rosws and cols for the 1st timestep and layer
-lon_list = np.array( mcip_input['LOND'][0][0][:][:])
-data = np.random.rand(265,250)
+lat_mesh = np.array( mcip_input['LATD'][0][0][:][:] ) # select only rosws and cols for the 1st timestep and layer
+lon_mesh = np.array( mcip_input['LOND'][0][0][:][:])
+data_mesh = np.random.rand(265,250)*10
 
 # plot dots from grid coordinates of the dots
-#plt.plot( lon_list , lat_list , marker='.' , color='b' , linestyle= 'none' )
+#plt.plot( lon_mesh , lat_mesh , marker='.' , color='b' , linestyle= 'none' )
 
 # plot the domain/region borders
-xcent = -120.805999755859 # degrees
-ycent = 40.0 # degrees
-NROWS = 266*1000 # meters
-NCOLS = 251*1000 # meters
+xcent =-120.806 # degrees
+ycent =40.000 # degrees
 
-bmap = bm( projection='lcc', lat_0= ycent , lon_0= xcent , height= NROWS , width= NCOLS )
-bmap.drawmapboundary(color='k' ) #, fill_color='aqua')
-bmap.drawcoastlines()#color = '0.15')
-bm.pcolormesh(lat_list , lon_list , data )
+NROWS = 265*1000 # meters
+NCOLS = 250*1000 # meters
+
+llcornerx=-117500 # meters
+llcornery=-265500 # meters
+
+urcornerx=132500 # meters
+urcornery=-500 # meters
+
+# create a Basemap class/model instance for a specific projection
+#basemap_instance = Basemap(projection='lcc' , lat_0=ycent , lon_0=xcent , height=NROWS , width=NCOLS , resolution='i') # , area_thresh=0.1) # latlon=True for when x and y are not in map proj. coordinates
+basemap_instance = Basemap(projection='lcc' ,
+                           llcrnrx=llcornerx , llcrnry=llcornery , urcrnrx=urcornerx , urcrnry=urcornery ,
+                           lat_0=ycent , lon_0=xcent , height=NROWS , width=NCOLS ,
+                           resolution='c')
+
+basemap_instance.bluemarble()
+
+x_mesh, y_mesh = basemap_instance(lon_mesh , lat_mesh) # order: x , y, transforms from degree to meter for LCC
+
+basemap_instance.drawmapboundary(color='k' ) #, fill_color='aqua')
+basemap_instance.drawcoastlines(color = '0.15')
+basemap_instance.drawcounties()
+basemap_instance.drawstates()
+
+#basemap_instance.fillcontinents(lake_color='aqua')
+
+im1 = basemap_instance.pcolormesh(x_mesh , y_mesh , data_mesh , cmap=plt.cm.Reds_r , shading='flat')
+#im2 = basemap_instance.pcolormesh(lon_mesh , lat_mesh , data_mesh , cmap=plt.cm.jet , shading='flat')
+
+cb = basemap_instance.colorbar(im1 , 'bottom')
+
+#cs = basemap_instance.contourf(lon_mesh , lat_mesh , data_mesh)
+#cbar = basemap_instance.colorbar(cs, location='bottom')
+
+
 plt.show()
