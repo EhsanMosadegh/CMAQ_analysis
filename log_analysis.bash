8d80a7bb473d17fdfadeaeb5796ace30307ac4f0
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-06-30 19:06:38 -0700

    updated the bash script to get the vmin/vmax

--- log_analysis.bash
+++ log_analysis.bash
@@ -1,7 +1,15 @@
 #!/bin/bash -f
 
-log_dir='/Users/ehsan/Documents/Python_projects/CMAQ_analysis/log_dir/'
+log_dir='/Users/ehsan/Documents/Python_projects/CMAQ_analysis/log_dir'
 #log_dir = '/storage/ehsanm/USFS_CA_WRF_1km_project/data_analysis/CMAQ_analysis/logs/'
+
+stats_property=min
+stats_pattern='vmin'
+log_file_pattern=log.o3*
+
+mon=10
+scenario=1
+output_file_name=$stats_pattern'_scen_'$scenario'_month_'$mon.txt
 
 current_dir=$(pwd)
 echo '-> we are currently at='
@@ -20,23 +28,62 @@ echo $work_dir
 echo '-> list of files at current dir='
 ls -la .
 
-echo '-> list of log files in work directory='
-log_list=$(ls *.txt)
+#echo '-> log files in work directory='
+
+#log_files=$(grep -irnH 'vmin' .)
+echo '-> get the list of log files in the log directory and write to file'
+ls $log_file_pattern > log_list.txt
+
+echo '-> get the number of lines in log list'
+wc -l log_list.txt
+
+# check and remove the file is it exist before
+echo '-> output file will be removed first:' $output_file_name
+rm ./$output_file_name
+
+echo '-> loop and read in log_list'
+while read log_list_line
+do 
+	echo '-> log file is=' $log_list_line
+	echo '-> capturing stats_pattern=' $stats_pattern
+
+	thePattern=$(grep -o $stats_pattern.* $log_list_line)
+	echo '-> extracting pattern=' $thePattern
+
+	grep -o $stats_pattern.* $log_list_line | cut -f2 -d' ' >> $output_file_name
+
+done < log_list.txt
+
+echo '-> size of the' $stats_pattern 'list is=' 
+cat $output_file_name | py -l 'print(len(l))'
+
+echo '-> absolute' $stats_property 'value from the file:' $output_file_name 'is='
+cat $output_file_name | py -l 'min(l)'
 
 
-echo $log_list
+#--- method 1 to read each line
 
+# echo '-> open file and do for loop to show the lines ...'
+#??? how open the file first?
 
+# for word in $log_files; do
 
-#foreach logfile ( $log_list ) 
+# 	echo the new word= $word
+# done
 
-#	echo '-> working on log file=' $logfile
+#--- method 2 to read each line
 
-#end
+# filename=$work_dir
+# n=1
+# echo '-> read file content line-by-line'
 
+# while read eachLogFile
+# do 
+# 	echo '-> line no.=' $n
+# 	echo '-> the line is=' $eachLogFile
+# 	new_stats_pattern=grep 'vmin=' $eachLogFile
+# 	echo $new_stats_pattern
+# 	#n=$((n+1))
+# 	let n=$n+1
 
-#set vmin_list = 'grep -irnH 'vmin' log*'
-
-#foreach log_line ( vmin_list )
-
-#	echo '-> line is=' $ log_line
+# done < logfile.txt
