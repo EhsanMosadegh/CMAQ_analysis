3c5ccb93eb5af8a9282bbfd4f13297943eec5610
Ehsan Mosadegh <ehsan.mosadegh@gmail.com>
2019-08-01 12:16:44 -0700

    changed day_count to normal, cos it is string

--- run_scripts/spatial_CMAQ_analysis_for_cluster.py
+++ run_scripts/spatial_CMAQ_analysis_for_cluster.py
@@ -1529,20 +1529,20 @@ def spatial_plotting_daily( cctm_process , ycent_zoom , xcent_zoom , NROWS_zoom 
 	if ( plot_method == 'single_plot' ) :
 		if( cctm_process == 'atm' ) :
 
-			fig_name = str(day_count)+'_'+cctm_process+'_conc_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_singlePlot_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days'+'_dpi_'+str(dpi_scale)+'.png'
+			fig_name = day_count+'_'+cctm_process+'_conc_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_singlePlot_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days'+'_dpi_'+str(dpi_scale)+'.png'
 		
 		if ( cctm_process == 'dep') :
 
-			fig_name = str(day_count)+'_'+dep_type+'_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_singlePlot_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days'+'_dpi_'+str(dpi_scale)+'.png'
+			fig_name = day_count+'_'+dep_type+'_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_singlePlot_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days'+'_dpi_'+str(dpi_scale)+'.png'
 
 	elif ( plot_method == 'diff_plot' ) :
 		if( cctm_process == 'atm' ) :
 
-			fig_name = str(day_count)+'_'+cctm_process+'_conc_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_diff_from_baseline_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days_day'+str(day_of_the_month)+'_dpi_'+str(dpi_scale)+'.png'
+			fig_name = day_count+'_'+cctm_process+'_conc_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_diff_from_baseline_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days_day'+str(day_of_the_month)+'_dpi_'+str(dpi_scale)+'.png'
 
 		if ( cctm_process == 'dep' ) :
 
-			fig_name = str(day_count)+'_'+dep_type+'_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_diff_from_baseline_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days_dpi_'+str(dpi_scale)+'.png'
+			fig_name = day_count+'_'+dep_type+'_'+cmaq_pol+'_dailyPlot_scen_'+scenario+'_diff_from_baseline_month_'+cmaq_file_month+'_summed_'+str(days_to_run_in_month)+'_days_dpi_'+str(dpi_scale)+'.png'
 	else:
 		pass
 
